3ce7f6b367e1573798fc251f2f2af250b48d6169
Denis Kolodin <deniskolodin@gmail.com>
2016-07-25 16:34:01 +0300

    Add mingw support

--- build.rs
+++ build.rs
@@ -25,41 +25,43 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-	let mut cfg = gcc::Config::new();
+    let mut cfg = gcc::Config::new();
     let env = env::var("TARGET").unwrap();
 
     cfg.file("nfd/src/nfd_common.c");
 
     if env.contains("darwin") {
-    	cfg.file("nfd/src/nfd_cocoa.m");
-    	cfg.compile("libnfd.a");
+        cfg.file("nfd/src/nfd_cocoa.m");
+        cfg.compile("libnfd.a");
         println!("cargo:rustc-link-lib=framework=AppKit");
     } else if env.contains("windows") {
-    	cfg.file("nfd/src/nfd_win.cpp");
-    	cfg.compile("libnfd.a");
+        cfg.file("nfd/src/nfd_win.cpp");
+        cfg.compile("libnfd.a");
         println!("cargo:rustc-link-lib=ole32");
+        // MinGW doesn't link it by default
+        println!("cargo:rustc-link-lib=uuid");
     } else {
-    	let pkg_output = Command::new("pkg-config")
-			.arg("--cflags")
-			.arg("gtk+-3.0")
-			.arg("glib-2.0")
-			.arg("--libs")
-			.arg("glib-2.0")
-			.output();
-    	match pkg_output {
-    		Ok(output) => {
-    			let t = String::from_utf8(output.stdout).unwrap();
-    			let flags = t.split(" ");
-    			for flag in flags {
-    				if flag != "\n" && flag != "" {
-    					cfg.flag(flag);
-    				}
-    			}
-    		}
-    		_ => (),
-    	}
-    	cfg.file("nfd/src/nfd_gtk.c");
-    	cfg.compile("libnfd.a");
+        let pkg_output = Command::new("pkg-config")
+            .arg("--cflags")
+            .arg("gtk+-3.0")
+            .arg("glib-2.0")
+            .arg("--libs")
+            .arg("glib-2.0")
+            .output();
+        match pkg_output {
+            Ok(output) => {
+                let t = String::from_utf8(output.stdout).unwrap();
+                let flags = t.split(" ");
+                for flag in flags {
+                    if flag != "\n" && flag != "" {
+                        cfg.flag(flag);
+                    }
+                }
+            }
+            _ => (),
+        }
+        cfg.file("nfd/src/nfd_gtk.c");
+        cfg.compile("libnfd.a");
         println!("cargo:rustc-link-lib=gdk-3");
         println!("cargo:rustc-link-lib=gtk-3");
         println!("cargo:rustc-link-lib=glib-2.0");
